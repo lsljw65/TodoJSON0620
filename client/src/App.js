59ecbc8eae90cc24f93aff17d95e5b0744c1bd69
lee <lsljw65@gmail.com>
2024-06-21 16:13:26 +0900

    immer방식

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import TodoList from "./component/TodoList";
 import NewTodoForm from "./component/NewTodoForm";
+import { produce } from "immer";
 
 function useTodoStatus() {
   const [todos, setTodos] = useState([]);
@@ -13,19 +14,36 @@ function useTodoStatus() {
       id,
       title,
     };
-    setTodos([...todos, newTodo]);
+    // setTodos([...todos, newTodo]);
+    setTodos(
+      produce(todos, (draft) => {
+        draft.push(newTodo);
+      })
+    );
     setLastTodoId(id);
   }
 
   function removeTodo(id) {
-    const newTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(newTodos);
+    // const newTodos = todos.filter((todo) => todo.id !== id);
+
+    // setTodos(newTodos);
+    setTodos(
+      produce(todos, (draft) => {
+        const index = draft.findIndex((todo) => todo.id === id);
+        draft.splice(index, 1);
+      })
+    );
   }
 
   function modifyTodo(id, title) {
-    const newTodos = todos.map((todo) =>
+    /* const newTodos = todos.map((todo) =>
       todo.id !== id ? todo : { ...todo, title }
-    );
+    ); */
+    const newTodos = produce(todos, (draft) => {
+      const index = draft.findIndex((todo) => todo.id === id);
+      draft[index].title = title;
+    });
+
     setTodos(newTodos);
   }
 
